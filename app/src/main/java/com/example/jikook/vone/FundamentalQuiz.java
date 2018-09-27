1ac44aeccaaff46d6a938c4404ff9b6d081a07a0
Edmond Lai <heihei19991218@yahoo.com.hk>
2018-09-27 14:50:30 +1000

    SSSSS  sep 20  6;05pm

--- app/src/main/java/com/example/jikook/vone/FundamentalQuiz.java
+++ app/src/main/java/com/example/jikook/vone/FundamentalQuiz.java
@@ -2,24 +2,46 @@ package com.example.jikook.vone;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class FundamentalQuiz extends AppCompatActivity {
     //this is the actual quiz class (topic 1 - fundamental)
 
 TextView mark;  //the textview that displays the mark dynamically
+TextView question;
+static int score = 0;
+static int questionNo = 1;
+static int correctAns = 0;
+static int wrongAns = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fundamental_quiz);
-
         mark = findViewById(R.id.mark);
-        mark.setText(0);   //display default mark of 0, change to different values when user answer correct or wrong
+        mark.setText(Integer.toString(score));   //display default mark of 0, change to different values when user answer correct or wrong
+
+       question = findViewById(R.id.questionBox);
+
+
+        String[] myArray = getResources().getStringArray(R.array.fund_quiz);
+
+        question.setText(myArray[0]);
+
+        for(;questionNo <= 10;questionNo++){
+
+            //each time user answer a questio, show them which question they are on and update the score
+        }
 
         //display quiz content (using fragment /arraylist??????)
         //
